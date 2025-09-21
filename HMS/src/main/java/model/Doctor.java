d8bd1df6368b8a8afad0989bc81c5762ce32545a
Long231005 <nguyenlongvu2020123@gmail.com>
2025-09-21 21:39:38 +0700

    base

---
+++ HMS/src/main/java/model/Doctor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+//This is sample guys
+/**
+ *
+ * @author ADMIN
+ */
+public class Doctor {
+    
+}
